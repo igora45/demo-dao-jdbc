36cfb31f25e5c8db9658075d1a02ef34f63e68c3
Igor Carvalho de Macedo Antônio <igordemacedo129@gmail.com>
2024-01-22 21:00:42 -0300

    feat: Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,16 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
         Department obj = new Department(1, "books");
-        System.out.println(obj);
+        Seller person = new Seller(1, "igor", "igor@gmail.com", new Date(), 5.500, obj);
+        System.out.println(person);
     }
 }
