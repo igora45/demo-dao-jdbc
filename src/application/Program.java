0ec16b8163578f008128ff298786ac1507672bc8
Igor Carvalho de Macedo Antônio <igordemacedo129@gmail.com>
2024-01-23 21:46:12 -0300

    feat: findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,11 +11,10 @@ import java.util.Date;
 public class Program {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-
-        Department obj = new Department(1, "books");
-        Seller person = new Seller(1, "igor", "igor@gmail.com", new Date(), 5.500, obj);
-        System.out.println(person);
         SellerDao sellerDao = DaoFactory.createSellerDao();
-        System.out.println(sellerDao);
+
+        Seller seller = sellerDao.findById(9);
+
+        System.out.println(seller);
     }
 }
